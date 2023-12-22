b1f981c15c1509d091e7ce184188779fcca64586
BattleBirdy <ortvin14@gmail.com>
2023-12-22 13:02:37 +0000

    Initial Commit

--- dump.sql
+++ dump.sql
@@ -7,13 +7,14 @@ CREATE TABLE `users` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
+  `userdescription` varchar(350) DEFAULT 'This user has not changed their description.',
   `isadmin` tinyint DEFAULT '0',
   PRIMARY KEY (`userID`),
   UNIQUE KEY `Username_UNIQUE` (`username`)
 );
 
 LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES (1,'BattleMod','password123','BattleMod@gmail.com',1),(2,'Battle','password123','Battle@gmail.com',0),(3,'B','B','Battle@gmail.com',0);
+INSERT INTO `users` VALUES (1,'BattleMod','password123','BattleMod@gmail.com','This user is a mod!',1),(2,'Battle','password123','Battle@gmail.com','This user is NOT a mod!',0);
 UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `topics`;
@@ -63,7 +64,7 @@ CREATE TABLE `posts` (
 );
 
 LOCK TABLES `posts` WRITE;
-INSERT INTO `posts` VALUES (1,1,1,'VIC','Volkolov Industries is a StarSector mod that adds many intresting ships that have a variety of unique game play affects','2023-12-15 16:38:31',NULL),(2,2,1,'beep','rgfer','2023-12-20 13:48:44',NULL),(3,2,1,'hj','berep','2023-12-20 13:52:56',NULL);
+INSERT INTO `posts` VALUES (1,1,1,'VIC','Volkolov Industries is a StarSector mod that adds many interesting ships that have a variety of unique game play affects','2023-12-15 16:38:31',NULL),(2,2,1,'Industrial Evolution','This mod adds many interesting colony and exploration affects!','2023-12-20 13:48:44',NULL),(3,2,1,'Diable Avionics','This mod adds many "carrier" focused ships to the game!','2023-12-20 13:52:56',NULL);
 UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `replies`;
